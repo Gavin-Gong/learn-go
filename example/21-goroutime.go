b1137787d1556165215b45ddc8d1610b08e66867
Kefen Gong <kefengong@gmail.com>
2018-03-28 12:54:11 +0800

    update

--- example/21-goroutime.go
+++ example/21-goroutime.go
@@ -1,12 +1,22 @@
 package example
 
-import _ "fmt"
+import . "fmt"
 
 /*
  * goroutine 本质上是一个轻量级的线程, 线程和进程的区别?
  * goroutime是go支持高并发的核心所在
- *
+ * goroutine 执行的函数会与非goroutine代码同时执行
  */
 func init() {
+	console("no goroutine")
+	go func(str string) {
+		console(str)
+	}("goroutine")
+	console("no goroutine")
+}
 
+func console(s string) {
+	for i := 0; i < 8; i++ {
+		Println(s, i)
+	}
 }
