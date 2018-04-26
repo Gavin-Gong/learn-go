478a3be8b18f27362f7c5f3398d6ac0e1235eb74
Gavin Gong <kefengong@gmail.com>
2018-04-26 23:53:16 +0800

    文件下载

--- main.go
+++ main.go
@@ -1,7 +1,8 @@
 package main
 
 // import "github.com/Gavin-Gong/learn-go/crawler"
-import _ "github.com/Gavin-Gong/learn-go/example"
+// import _ "github.com/Gavin-Gong/learn-go/example"
+import _ "github.com/Gavin-Gong/learn-go/practice/downloader"
 
 func main() {
 	// crawler.Start()
---
+++ practice/downloader/downloader.go
@@ -0,0 +1,38 @@
+package downloader
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func init() {
+	DownLoad("./xx.jpg", "http://i.meizitu.net/2018/04/25c01.jpg")
+}
+func DownLoad(path string, url string) error {
+	out, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	// get date by url
+	client := &http.Client{}
+	req, _ := http.NewRequest("GET", url, nil)
+	req.Header.Set("Referer", url)
+
+	res, err := client.Do(req)
+	defer res.Body.Close()
+	if err != nil {
+		return err
+	}
+
+	// 写入文件
+	_, err = io.Copy(out, res.Body)
+	if err != nil {
+		return err
+	}
+	fmt.Println("xxx")
+	return nil
+}
