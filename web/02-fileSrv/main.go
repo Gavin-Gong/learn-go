8e1aa8160a57174df9469822f0b530d0aaa47068
Gavin Gong <kefengong@gmail.com>
2018-05-08 22:38:22 +0800

    update fileSrv

--- web/02-fileSrv/main.go
+++ web/02-fileSrv/main.go
@@ -1,9 +1,19 @@
 package fileSrv
 
 import (
+	"fmt"
+	"net"
 	"net/http"
 )
 
 func Start() {
+	addrs, _ := net.InterfaceAddrs()
+	for _, address := range addrs {
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				fmt.Println(ipnet.IP.String() + ":8080")
+			}
+		}
+	}
 	http.ListenAndServe(":8080", http.FileServer(http.Dir("/Go/src")))
 }
